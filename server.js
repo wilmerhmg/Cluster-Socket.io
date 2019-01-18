607766ee0531925f5c09fc31d5f476937fcc5505
Wilmer H. Muñoz <wilmermg@outlook.com>
2019-01-18 17:31:36 -0500

    Redis Implementation

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const io    = require('socket.io')(3002);
-const redis = require('socket.io-redis');
-io.adapter(redis({host: 'localhost', port: 32769}));
+import express from 'express';
+import path    from 'path';
+import http    from 'http';
+import * as ws from 'socket.io';
 
-io.emit('hi-event','Hello world');
+const redis  = require('socket.io-redis');
+const app    = express();
+const server = http.createServer({}, app);
+const IO     = ws.listen(server);
+const PORT   = process.env.PORT || 8086;
+
+app.use(express.static(path.join(__dirname, 'slim-frontend')));
+IO.adapter(redis({host: 'localhost', port: 32769}));
+
+IO.on('connection', function (socket) {
+
+  socket.on('PublicRoom', function (data) {
+    IO.emit('news', data);
+  });
+
+});
+
+server.listen(PORT, function () {
+  console.log(`Server listening: http://0.0.0.0:${PORT}`);
+});
